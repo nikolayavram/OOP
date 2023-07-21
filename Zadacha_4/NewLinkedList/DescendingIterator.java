f0e4b825b7dfb2b83e98abafc1fb26b384fe492b
Nikolay <kolune4ka@mail.ru>
2023-07-21 15:05:32 +0300

    Add OOP_4

---
+++ Zadacha_4/NewLinkedList/DescendingIterator.java
@@ -0,0 +1,7 @@
+package NewLinkedList;
+
+import java.util.Iterator;
+
+public interface DescendingIterator<E> {
+    Iterator<E> descendingIterator();
+}
---
+++ Zadacha_4/NewLinkedList/Linked.java
@@ -0,0 +1,8 @@
+package NewLinkedList;
+
+public interface Linked<E> {
+    void addLast(E e);
+    void addFirst(E e);
+    int size();
+    E getElementByIndex(int counter);
+}
